e9835aaaf5aa4e70f4f276799b6665c9a0fc26b2
nmigacz <nickmigacz@gmail.com>
2020-11-14 10:50:34 -0700

    Working homepage and beginning of adding on pages

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ let dvd = {
     pickColor();
     update();
     menuClick();
+    document.body.style.background = 'index.html(' + canvas.toDataURL() + ')';
 })();
 
 function update() {
@@ -34,9 +35,6 @@ function update() {
         //Draw the canvas background
         ctx.fillStyle = '#050506';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.font = "30px Arial";
-        ctx.fillStyle = "white";
-        ctx.fillText = ("Hello World", 10, 50);
 
         //Draw DVD Logo and his background
         ctx.fillStyle = logoColor;
@@ -79,6 +77,32 @@ function menuClick(){
     };
 }
 
+
+// Get the modal
+var modal = document.getElementById("myModal" || "myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn" || "myBTn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close" || "close2")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 /*
 let x;
